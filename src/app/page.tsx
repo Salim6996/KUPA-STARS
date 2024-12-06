d9998d47a4f790d90aff7581c3ab18b1fd159ff3
Salim6996 <156449458+Salim6996@users.noreply.github.com>
2024-12-07 00:37:05 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,35 +9,11 @@ import Hero from "@/components/cards/hero";
 export const metadata: Metadata = {
   title: "KUPA STARS",
   description: "Moderasyon, Eğlence, Oyun, Kullanıcı Komutları, Log Sistemi ve daha fazlasını içeren Discord Botu.",
-  // Thumbnail ve logo ekleniyor
-  openGraph: {
-    images: [
-      {
-        url: "https://i.hizliresim.com/776do5x.png", // Thumbnail resminin linki
-        width: 1200,
-        height: 630,
-      },
-    ],
-  },
-  twitter: {
-    card: "summary_large_image",
-    image: "https://i.hizliresim.com/776do5x.png", // Thumbnail resminin linki
-  },
 };
 
 export default function Home() {
   return (
     <div>
-      {/* Site logosu ekleme */}
-      <div className="flex justify-center mb-6">
-        <Image
-          src="https://i.hizliresim.com/776do5x.png" // Logo dosyasının linki
-          alt="KUPA STARS Logo"
-          width={150} // İstediğiniz genişlik
-          height={150} // İstediğiniz yükseklik
-        />
-      </div>
-
       <Hero />
       <Servers />
       <Features />
@@ -45,4 +21,4 @@ export default function Home() {
       <InviteBot />
     </div>
   );
-      }
+}
