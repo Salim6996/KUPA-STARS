5edddfa4eab9ce3d4693b4b6eb521d665ab1c72a
Salim6996 <156449458+Salim6996@users.noreply.github.com>
2024-12-06 23:50:22 +0300

    Update partners.tsx

--- src/components/cards/partners.tsx
+++ src/components/cards/partners.tsx
@@ -7,8 +7,8 @@ const Partners: React.FC = () => {
       <div className="container mx-auto">
         <div className="flex flex-col items-center">
           <h2 className="text-2xl font-bold mb-6">
-            A lot of popular servers use{" "}
-            <span className="text-pink-500">Discord Bot Name</span>
+            Birçok popüler sunucu{" "}
+            <span className="text-pink-500">KUPA STARS</span>'ı kullanıyor
           </h2>
           <div className="flex flex-wrap justify-center gap-4">
             <Skeleton className="w-16 h-16 rounded-full" />
